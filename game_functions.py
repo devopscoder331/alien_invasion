f1190c37febab4b23a815b9fc8eb3a3fc45a33fb
devopscoder331 <devopscoder31@gmail.com>
2020-05-04 21:18:50 +0300

    add event keydown/keyup and move ship

--- game_functions.py
+++ game_functions.py
@@ -4,11 +4,27 @@ import sys
 
 import pygame
 
-def check_events():
+def check_events(ship):
     '''respond to keypress and mouse event'''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        # action if key down (move right/left)
+        elif event.type == pygame.KEYDOWN:
+            # move right
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = True\
+            # move left
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
+        # action if key up (move right/left)
+        elif event.type == pygame.KEYUP:
+            # stop move right
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            # stop move left
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 def update_screen(ai_settings, screen, ship):
     '''update display and flip to the new screen'''
