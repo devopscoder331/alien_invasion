995427b6d278b39aed9dc8a3983a1545b8b2496f
devopscoder331 <devopscoder31@gmail.com>
2020-05-05 17:48:25 +0300

    add class store alien obejct

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 
@@ -15,15 +16,15 @@ def run_game():
     # screen is name 'suraface'
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_width))
     pygame.display.set_caption("Alien Invasion")
-    # create the ship
+    # create the ship, alien, bullets
     ship = Ship(ai_settings, screen)
-    #create bullets Group
+    alien = Alien(ai_settings, screen)
     bullets = Group()
     # run main cycle game
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -39,7 +39,7 @@ def check_events(ai_settings, screen, ship, bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, alien, bullets):
     '''update display and flip to the new screen'''
     screen.fill(ai_settings.bg_collor)
     # bullet
@@ -47,6 +47,7 @@ def update_screen(ai_settings, screen, ship, bullets):
         bullet.draw_bullet()
     # draw a ship after screen.fill
     ship.blitme()
+    alien.blitme()
     pygame.display.flip()
 
 def fire_bullets(ai_settings, screen, ship, bullets):
